36cd4eed1eee4fc770e66db02d4a0949dadc7d59
pujitha-karra <pujithareddykarra386@gmail.com>
2026-05-05 12:26:57 +0530

    Create EvenOdd.java

---
+++ Day-01/EvenOdd.java
@@ -0,0 +1,16 @@
+mport java.util.*;
+public class EvenOdd{ 
+    public static void main(String[] args){ 
+    Scanner puji=new Scanner(System.in);
+    System.out.println("Enter a num:");
+    int num=puji.nextInt();
+if(num%2==0)
+{
+    System.out.println("num is Even");
+}
+else
+{
+    System.out.println("num is Odd");
+}
+} 
+}
