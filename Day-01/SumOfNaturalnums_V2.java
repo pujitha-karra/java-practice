9cc1dcdedcde79bc4816979dcb6052da869f4074
pujitha-karra <pujithareddykarra386@gmail.com>
2026-05-05 12:45:08 +0530

    Create SumOfNaturalnums_V2.java

---
+++ Day-01/SumOfNaturalnums_V2.java
@@ -0,0 +1,8 @@
+public class SumOfNaturalnums{ 
+public static void main(String[] args){
+    int n = 10 ;
+    int sum = n*(n+1)/2;
+    System.out.println("sum of first " + n + " natural nums :" + sum );
+
+}
+}
