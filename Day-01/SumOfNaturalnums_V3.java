cf0ec131b6b466f773b91674c28c1972010a0749
pujitha-karra <pujithareddykarra386@gmail.com>
2026-05-05 12:48:46 +0530

    Create SumOfNaturalnums_V3.java

---
+++ Day-01/SumOfNaturalnums_V3.java
@@ -0,0 +1,10 @@
+public class SumOfNaturalnums{
+    public static void main(String[] args){
+        int n = 10;
+        int sum = 0;
+        for(int i=0;i<=10;i++){
+            sum = sum + i;
+            System.out.println("sum of fist " +n+" natural nums" + sum);
+        }
+    }
+}
