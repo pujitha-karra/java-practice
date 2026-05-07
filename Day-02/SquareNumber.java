3da4d1edf32d074810f36f95535978c9c1aab984
pujitha-karra <pujithareddykarra386@gmail.com>
2026-05-07 08:19:06 +0530

    Create SquareNumber.java

---
+++ Day-02/SquareNumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SquareNumber {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a num :");
+        int n = sc.nextInt();
+        int sqr = (n*n);
+        System.out.println("sqare num of " +  n + " is :" + sqr);
+    }
+}
